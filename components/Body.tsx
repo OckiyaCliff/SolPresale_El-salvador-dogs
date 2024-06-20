3722e549992e7587d0df047108ca86e7ab6a731c
Cliff <Ockiyacliffc@gmail.com>
2024-06-20 18:43:06 +0100

    info update

--- components/Body.tsx
+++ components/Body.tsx
@@ -8,16 +8,18 @@ interface BodyProps {
 
 const Body: React.FC<BodyProps> = ({ sendTransaction, isWalletConnected }) => {
   return (
-    <main className="flex flex-col items-center justify-center bg-black p-10 min-h-screen">
+    <main className="flex flex-col items-center justify-center bg-black p-10 min-h-screen relative">
+      <div className="inset-0 circlePosition w-[300px] h-[200px] bg-[#940ebd] rounded-[100%] absolute z-1 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] blur-[200px]"></div>
       {isWalletConnected ? (
-        <div className="relative p-10 rounded-sm shadow-lg">
-          <div className="inset-0 circlePosition w-[300px] h-[200px] bg-[#940ebd] rounded-[100%] absolute z-1 top-[90%] left-[80%] translate-x-[-50%] translate-y-[-50%] blur-[200px]"></div>
+        <div className="relative p-10 rounded-sm shadow-lg z-10">
           <div className="relative z-10 p-10">
             <SendTokenForm sendTransaction={sendTransaction} />
           </div>
         </div>
       ) : (
-        <p className="text-xl text-white">Please connect your wallet to send tokens.</p>
+        <div className="relative p-10 rounded-sm shadow-lg z-10">
+          <p className="text-xl text-white">Please connect your wallet to send tokens.</p>
+        </div>
       )}
     </main>
   );
--- components/SendTokenForm.tsx
+++ components/SendTokenForm.tsx
@@ -15,25 +15,31 @@ const SendTokenForm: React.FC<SendTokenFormProps> = ({ sendTransaction }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-10 bg-gray-100 bg-opacity-5 border border-gray-700 p-6 rounded-lg">
-      <div>
-        <label className="block text-gray-700">El-salvador dogs presale Address:</label>
-        <p>{fixedRecipient}</p>
+    <div>
+      <div className='space-y-10 bg-gray-100 bg-opacity-5 border border-gray-700 p-6 rounded-lg my-2'>
+        El-savador Dogs
       </div>
-      <div>
-        <label className="block text-gray-700">Amount:</label>
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-          required
-          className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
-        />
-      </div>
-      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-        Send Tokens
-      </button>
-    </form>
+      <form onSubmit={handleSubmit} className="space-y-10 bg-gray-100 bg-opacity-0 border border-gray-700 p-6 rounded-lg">
+        <div>
+          <label className="block text-gray-200 py-2">El-salvador dogs presale Address:</label>
+          <p className="text-gray-400">{fixedRecipient}</p>
+        </div>
+        <div>
+          <label className="block text-gray-200 py-2">Amount:</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+            required
+            className="mt-1 p-2 block w-full bg-transparent border border-gray-600 rounded-md text-gray-200"
+          />
+        </div>
+        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg ">
+          Send Tokens
+        </button>
+      </form>
+    </div>
+    
   );
 };
 
