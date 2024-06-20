f225966840fc16b9e1d05b4cf3207a999a3dcfb3
Milton Black <arziblack2@gmail.com>
2024-06-20 22:24:43 +0100

    feat: used moment to get time difference and format time properly...

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,4 +1,5 @@
 "use client";
+import moment from "moment";
 import SendTokenForm from "./SendTokenForm";
 import { Timer } from "./Timer";
 
@@ -8,8 +9,10 @@ interface BodyProps {
 }
 
 const Body: React.FC<BodyProps> = ({ sendTransaction, isWalletConnected }) => {
-  const time = new Date();
-  time.setSeconds(time.getSeconds() + 600);
+  const now = moment();
+  const futureDate = moment('2024-12-31T23:59:59');
+  const duration = moment.duration(futureDate.diff(now));
+  const expiryTimestamp = new Date(now.add(duration).toISOString());
   return (
     <main className="flex flex-col items-center justify-center bg-black p-4 sm:p-10 min-h-screen relative">
       <div className="inset-0 circlePosition w-[300px] h-[200px] bg-[#c507ff] rounded-full absolute z-0 top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 blur-[170px]"></div>
@@ -18,9 +21,9 @@ const Body: React.FC<BodyProps> = ({ sendTransaction, isWalletConnected }) => {
           <SendTokenForm sendTransaction={sendTransaction} />
         </div>
       ) : (
-        <div className="relative p-6 sm:p-10 rounded-sm shadow-lg z-10 bg-opacity-75">
-          <Timer expiryTimestamp={time} />
-          <p className="text-xl text-white">
+        <div className="relative p-6 sm:p-10 rounded-sm shadow-lg z-10 bg-opacity-75 flex flex-col items-center justify-center">
+          <Timer expiryTimestamp={expiryTimestamp} />
+          <p className="text-xl text-white pt-5">
             Please connect your wallet to Buy.
           </p>
         </div>
