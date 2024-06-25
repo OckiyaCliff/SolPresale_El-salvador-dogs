884ac0ae42a99eb5b2a85a1b2a6d1fdd0ba6e2df
Cliff <Ockiyacliffc@gmail.com>
2024-06-25 17:20:23 +0100

    x link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex justify-center items-center py-4 bg-black text-white">
       <div className="flex space-x-4">
-        <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-400">
+        <a href="https://x.com/elsalvadoronsol?t=Fq_DcPmZdhwehRgt3cDKMA&s=09" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-400">
           <FaTwitter size={30} />
         </a>
         <a href="https://t.me/Elsalvadordogss" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-400">
